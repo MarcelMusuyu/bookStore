a6c0704f1a4437589f739b3fce3eab3320881c40
Marcel Nyembo <marcelmusuyu9@gmail.com>
2025-04-08 12:25:54 +0000

    updating documentation

--- utilities/index.js
+++ utilities/index.js
@@ -4,7 +4,7 @@ const dbConnect = require('../database/'); // Import dbConnect.js
 const Util= {};
 Util.getModel= async function (dbName, schema, modelName) {
   const connection = await dbConnect(dbName);
-  return connection.model(modelName, schema);
+  return connection.model(modelName, schema,modelName);
 }
 
 module.exports= Util
